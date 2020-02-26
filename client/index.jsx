7912990a7c051b20bc1c659de75cb4892206a0b3
Puyan Wei <puyanwei@gmail.com>
2020-02-26 00:04:57 +0000

    Linked up Query to component allowing data to be available in props

--- client/index.jsx
+++ client/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
+import SongList from "./components/songlist";
 
 const client = new ApolloClient({});
 
 const Root = () => (
   <ApolloProvider client={client}>
-    <div>Lyrical</div>
+    <SongList />
   </ApolloProvider>
 );
 
