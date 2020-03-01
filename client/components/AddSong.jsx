cdd6ef1078af9e4cda2b24767dc87e6cc018b676
Puyan Wei <puyanwei@gmail.com>
2020-03-01 17:13:21 +0000

    Addd AddSong mutation to app

--- client/components/AddSong.jsx
+++ client/components/AddSong.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-const AddSong = () => (
-  <div>
-    <h3>This is the adding songs page</h3>
-  </div>
-);
+class AddSong extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "" };
+  }
 
-export default AddSong;
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Add in a new song</h3>
+        <form onSubmit={e => this.onSubmit(e)}>
+          <label>Song Title:</label>
+          <input
+            onChange={e => this.setState({ title: e.target.value })}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(AddSong);
